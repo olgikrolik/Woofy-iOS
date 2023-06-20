456fa0b7e1bdd34cb3852793bd1969f97d1325cd
Olga Królikowska <olga.szydlowska2@gmail.com>
2023-06-20 08:40:18 +0200

    Initial Commit

---
+++ Woofy/WoofyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoofyApp.swift
+//  Woofy
+//
+//  Created by Olga Królikowska on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WoofyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
