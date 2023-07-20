73a02ff999ca2d66b307c81cce2284f7b2f9ebde
Olga Królikowska <olga.szydlowska2@gmail.com>
2023-07-20 23:13:59 +0200

    Refactor Breed Details View

--- Woofy/Views/BreedDetailsView.swift
+++ Woofy/Views/BreedDetailsView.swift
@@ -10,7 +10,7 @@ import WrappingHStack
 
 struct BreedDetailsView: View {
     
-    let breedTitleHeight = 114
+    let breedTitleHeight: CGFloat = 114
     let temperamentArray = ["Stubborn", "Curious", "Playful", "Adventurous", "Active", "Fun-loving"]
     
     var body: some View {
@@ -18,132 +18,125 @@ struct BreedDetailsView: View {
             let imageWidth = UIScreen.main.bounds.size.width
             let imageHeight = imageWidth * 5/6
             let horizontalPadding: CGFloat = 24
-                ZStack(alignment: .bottom) {
-                    AsyncImage(url: URL(string: "https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg")) { image in
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .cornerRadius(5)
-                            .frame(width: imageWidth, height: imageHeight)
-                    } placeholder: {
-                        Image("WoofyDefaultImage")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .cornerRadius(5)
-                            .frame(width: imageWidth, height: imageHeight)
-                    }
-                    RoundedRectangle(cornerRadius: 30)
-                        .foregroundStyle(.ultraThinMaterial)
-                        .shadow(radius: 20)
-                        .opacity(0.9)
-                        .frame(height: CGFloat(breedTitleHeight))
-                        .padding(.horizontal, horizontalPadding)
-                        .offset(y: CGFloat(breedTitleHeight * 1/2))
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("Affenpinscher")
-                                .font(.custom("Trocchi-Regular", size: 20))
-                            Text("Group:")
-                                .font(.custom("Trocchi-Regular", size: 15))
-                                .opacity(0.7)
-                            Text("Special skill:")
-                                .font(.custom("Trocchi-Regular", size: 15))
-                                .opacity(0.7)
-                        }
-                        .padding(.horizontal, horizontalPadding)
-                        Spacer()
-                    }
-                    .padding(.horizontal, horizontalPadding)
-                    .frame(height: CGFloat(breedTitleHeight), alignment: .leading)
-                    .offset(y: CGFloat(breedTitleHeight * 1/2))
-                }
-                .frame(width: imageWidth, height: imageHeight + CGFloat((breedTitleHeight * 1/2)), alignment: .top)
+            
+            ZStack(alignment: .bottom) {
+                breedImage(imageWidth: imageWidth, imageHeight: imageHeight)
+                backgroundForBreedIntroduction(horizontalPadding: horizontalPadding)
+                breedIntroduction(horizontalPadding: horizontalPadding)
+            }
+            .frame(width: imageWidth, height: imageHeight + CGFloat((breedTitleHeight * 1/2)), alignment: .top)
+            
+            sectionWithImage(horizontalPadding: horizontalPadding, title: "About breed")
             
             HStack {
-                Image("WoofySmallIcon")
-                    .resizable()
-                    .frame(width: 30, height: 45)
-                Text("About breed")
-                    .font(.custom("Trocchi-Regular", size: 15))
-                    .padding(.leading, 10)
-                Spacer()
+                createTile(title: "Weight", value: "3-6 kg")
+                createTile(title: "Height", value: "23-29 cm")
+                createTile(title: "Life span", value: "10-12 yrs")
             }
             .padding(.horizontal, horizontalPadding + 8)
             .padding(.top, 15)
             
-            HStack {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 30)
-                        .foregroundColor(.accentColor)
-                        .frame(height: 100)
-                    VStack {
-                        Text("Weight")
-                            .font(.custom("Trocchi-Regular", size: 15))
-                        Text("3-6 kg")
-                            .font(.custom("Trocchi-Regular", size: 15))
-                            .foregroundColor(Color("DetailsColor"))
-                            .padding(.top, 1)
-                    }
-                }
-                ZStack {
-                    RoundedRectangle(cornerRadius: 30)
-                        .foregroundColor(.accentColor)
-                        .frame(height: 100)
-                    VStack {
-                        Text("Height")
-                            .font(.custom("Trocchi-Regular", size: 15))
-                        Text("23-29 cm")
-                            .font(.custom("Trocchi-Regular", size: 15))
-                            .foregroundColor(Color("DetailsColor"))
-                            .padding(.top, 1)
-                    }
-                }
-                ZStack {
-                    RoundedRectangle(cornerRadius: 30)
-                        .foregroundColor(.accentColor)
-                        .frame(height: 100)
-                    VStack {
-                        Text("Life span")
-                            .font(.custom("Trocchi-Regular", size: 15))
-                        Text("10-12 yrs")
-                            .font(.custom("Trocchi-Regular", size: 15))
-                            .foregroundColor(Color("DetailsColor"))
-                            .padding(.top, 1)
-                    }
-                }
-            }
-            .padding(.horizontal, horizontalPadding + 8)
-            .padding(.top, 15)
-            
-            HStack {
-                Image("WoofySmallIcon")
-                    .resizable()
-                    .frame(width: 30, height: 45)
-                Text("Temperament")
-                    .font(.custom("Trocchi-Regular", size: 15))
-                    .padding(.leading, 10)
-                Spacer()
-            }
-            .padding(.horizontal, horizontalPadding + 8)
-            .padding(.top, 15)
-            
-            WrappingHStack(temperamentArray, id:\.self, alignment: .center) { temperament in
-                Text(temperament)
-                    .font(.custom("Trocchi-Regular", size: 15))
-                    .padding(.all, 8)
-                    .opacity(0.7)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 30)
-                            .stroke(Color("DetailsColor"), lineWidth: 1)
-                    )
-                    .padding(3)
-            }
-            .padding(.horizontal, horizontalPadding + 8)
-            .padding(.top, 8)
+            sectionWithImage(horizontalPadding: horizontalPadding, title: "Temperament")
+            temperamentalFeatures(temperamentArray: temperamentArray, horizontalPadding: horizontalPadding)
             Spacer()
         }
     }
+    
+    
+    func breedImage(imageWidth: CGFloat, imageHeight: CGFloat) -> some View {
+        AsyncImage(url: URL(string: "https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg")) { image in
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .cornerRadius(5)
+                .frame(width: imageWidth, height: imageHeight)
+        } placeholder: {
+            Image("WoofyDefaultImage")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .cornerRadius(5)
+                .frame(width: imageWidth, height: imageHeight)
+        }
+    }
+    
+    func backgroundForBreedIntroduction(horizontalPadding: CGFloat) -> some View {
+        RoundedRectangle(cornerRadius: 30)
+            .foregroundStyle(.ultraThinMaterial)
+            .shadow(radius: 20)
+            .opacity(0.9)
+            .frame(height: breedTitleHeight)
+            .padding(.horizontal, horizontalPadding)
+            .offset(y: CGFloat(breedTitleHeight * 1/2))
+    }
+    
+    func breedIntroduction(horizontalPadding: CGFloat) -> some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Affenpinscher")
+                    .font(.custom("Trocchi-Regular", size: 20))
+                Text("Group:")
+                    .font(.custom("Trocchi-Regular", size: 15))
+                    .opacity(0.7)
+                Text("Special skill:")
+                    .font(.custom("Trocchi-Regular", size: 15))
+                    .opacity(0.7)
+            }
+            .padding(.horizontal, horizontalPadding)
+            Spacer()
+        }
+        .padding(.horizontal, horizontalPadding)
+        .frame(height: CGFloat(breedTitleHeight), alignment: .leading)
+        .offset(y: CGFloat(breedTitleHeight * 1/2))
+    }
+    
+    func sectionWithImage(horizontalPadding: CGFloat, title: String) -> some View {
+        HStack {
+            Image("WoofySmallIcon")
+                .resizable()
+                .frame(width: 30, height: 45)
+            Text(title)
+                .font(.custom("Trocchi-Regular", size: 15))
+                .padding(.leading, 10)
+            Spacer()
+        }
+        .padding(.horizontal, horizontalPadding + 8)
+        .padding(.top, 15)
+    }
+    
+    func createTile(title: String, value: String) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 30)
+                .foregroundColor(.accentColor)
+                .frame(height: 100)
+            VStack {
+                Text(title)
+                    .font(.custom("Trocchi-Regular", size: 15))
+                Text(value)
+                    .font(.custom("Trocchi-Regular", size: 15))
+                    .foregroundColor(Color("DetailsColor"))
+                    .padding(.top, 1)
+            }
+        }
+    }
+    
+    func temperamentalFeatures(temperamentArray: [String], horizontalPadding: CGFloat) -> some View {
+        WrappingHStack(temperamentArray, id:\.self, alignment: .center) { temperament in
+            Text(temperament)
+                .font(.custom("Trocchi-Regular", size: 15))
+                .padding(.all, 8)
+                .opacity(0.7)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(Color("DetailsColor"), lineWidth: 1)
+                )
+                .padding(3)
+        }
+        .padding(.horizontal, horizontalPadding + 8)
+        .padding(.top, 8)
+    }
+    
 }
+
 
 struct BreedDetailsView_Previews: PreviewProvider {
     static var previews: some View {
